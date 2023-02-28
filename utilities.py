a0c18c9fcc1b41e065e83e368976a9b621fc3dce
dlolis <dlolis88@gmail.com>
2023-02-28 16:20:11 +0200

    skeleton of the utils

--- utilities.py
+++ utilities.py
@@ -1,3 +1,21 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+"""
+The main function of this  module is to contain several funtions
+to remove possibly too complicated code from the jupyter notebook.
+Depending on how much they share data i might transform it into a class.
+"""
+
+def fn_1():
+    pass
+
+
+def fn2():
+    pass
+
+
+def fn3():
+    pass
 
